3d98566243bdcf4e1306263931e7f8b159581aa8
Deekshith M D <deekshithmogra@gmail.com>
2021-12-06 19:29:27 +0530

    refactor: fixes

--- app.js
+++ app.js
@@ -7,35 +7,38 @@ var message=document.querySelector("#message")
 
 const note=[2000, 500, 100, 20, 10, 5, 1]
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", function billCheck(){
     //console.log(bill);
     //console.log(cash);
-    if(bill_amount.value>0){
-        if(cash_given.value>=bill_amount.value){
+    hideMessage()
+    if(bill_amount.value > 0){
+        if(cash_given.value >= bill_amount.value){
             const remainingChange=cash_given.value - bill_amount.value
             calculateChange(remainingChange)
-            hideMessage()
         }
         else{
-            showMessage()
+            //console.log("less")
+            //showMessage("Please pay your bill amount")
+            alert("Please pay proper bill amount...")
         }
+    }
+    else{
+        showMessage("Please enter bill details...")
     }
 })
 
 function calculateChange(remainingChange){
-    for(var i=0; note.length; i++){
-        const numberofNotes=Math.trunc(remainingChange/note[i])
-        remainingChange=remainingChange%note[i]
-        console.log(numberofNotes)
+    for(var i=0; i<note.length; i++){
+        const numberofNotes=Math.trunc(remainingChange / note[i])
+        remainingChange=remainingChange % note[i]
         if(numberofNotes>0)
             notes[i].innerText=numberofNotes
-        else
-        notes[i].innerText=" "
     }
 }
-function showMessage(){
+
+function showMessage(msg){
     message.style.display="block"
-    message.innerText="Please pay your bill amount..."
+    message.innerText=msg
 }
 
 function hideMessage(){
